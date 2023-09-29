e0ee5b41c6d3a7396de93c7a0a4fded7d44291a2
amadad <ali@scty.org>
2023-09-29 14:41:59 -0400

    POC agentcy

--- README.md
+++ README.md
@@ -1,29 +1,34 @@
 # Multi-Agent Collaboration with AutoGen
 
+<p align="center">
+  <img src='./misc/logo1.png' width=600>
+</p>
+
 This code demonstrates the power of multi-agent collaboration using the AutoGen library. Instead of relying on a single agent to handle tasks, multiple specialized agents work together, each bringing its expertise to the table.
 
-## Overview
+## 📖 Overview
 
 The code sets up a collaborative environment where multiple agents, each with its unique role and expertise, come together to discuss, plan, and execute tasks. This collaboration ensures that different aspects of a task are handled by the most qualified agent, leading to more efficient and accurate outcomes.
 
-## Agents
+## 🕵🏽 Agents
 
 Here are the agents involved in the collaboration:
 
-1. **Admin (User Proxy)**: Represents the human user. Interacts with the planner to discuss and approve the plan.
-2. **Fun Manager**: Focuses on maximizing fun and optimizing for unique memorable experiences.
-3. **Gym Trainer**: Ensures the user maintains a healthy lifestyle by recommending training and diet plans.
-4. **Executive Assistant**: Prioritizes daily work tasks, ensuring they are completed before any recreational activities.
-5. **Planner**: Suggests and revises plans based on feedback from other agents and the admin.
-6. **Critic**: Reviews the plan to ensure all objectives are met and provides feedback.
+1. **Client**: Represents the human user. Interacts with the planner to discuss and approve the plan.
+2. **Strategist**: Drafts strategic briefs that effectively position the client's brand in the market based on comprehensive research and insights.
+3. **Researcher**: Delves deep into understanding user pain points, identifies market opportunities, and analyzes prevailing market conditions.
+4. **Marketer**: Transforms strategy and insights into compelling marketable ideas that resonate with the target audience.
+5. **Manager**: Oversees the entire project lifecycle, ensuring all agents are effectively fulfilling their objectives and tasks on time.
+6. **Director**: Guides the creative vision of the project, ensuring ideas are unique, compelling, and meet the highest standards of excellence.
 
-## Collaboration Flow
+
+## 🤝 Collaboration Flow
 
 1. The `GroupChat` class is used to create a collaborative environment where all agents can communicate.
 2. The `GroupChatManager` manages the group chat, ensuring smooth communication between agents.
 3. The `initiate_chat` method is called to start the collaboration. In this example, the task is to plan a month-long trip to Bangkok.
 
-## Setup & Configuration
+## ⚙️ Setup & Configuration
 
 1. Ensure you have the required libraries installed:
 pip install openai autogen
@@ -35,6 +40,6 @@ pip install openai autogen
 
 4. Create a group chat with all the agents and initiate the collaboration.
 
-## Conclusion
+## ⏯️ Conclusion
 
 This multi-agent collaboration approach allows for more comprehensive and efficient task handling. By leveraging the expertise of multiple agents, we can ensure that every aspect of a task is addressed by the most qualified entity. Whether it's planning a trip, as demonstrated in this example, or any other task, this collaborative approach can be adapted to fit various scenarios.
--- main.py
+++ main.py
@@ -1,54 +1,92 @@
-import os
-import openai
 import autogen
 from autogen import AssistantAgent, UserProxyAgent, config_list_from_json
 
+# Prompt the user for input
+user_task = input("Please enter the task for the new client's campaign brief: ")
+
 config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
 
-user_proxy = autogen.UserProxyAgent(
-    name="Admin",
-    system_message="A human admin. Interact with the planner to discuss the plan. Plan execution needs to be approved by this admin.",
+client = autogen.UserProxyAgent(
+    name="Client",
+    system_message="A human client. Interact with the planner to discuss the strategy. Plan execution needs to be approved by this client.",
     code_execution_config=False,
 )
 
-fun_engineer = autogen.AssistantAgent(
-    name="Fun_Manager",
+agency_strategist = autogen.AssistantAgent(
+    name="Agency_Strategist",
     llm_config={"config_list": config_list},
-    system_message='''Fun Manager. You maximize the fun when Admin is at a location - optimize for unique memorable experiences & fun stories.'''
+    system_message=f'''
+    You are the Lead Strategist.
+    Your primary responsibility is to draft strategic briefs that effectively position our client's brand in the market.
+    Based on the information provided in: {user_task}, your task is to craft a comprehensive strategic brief that outlines the brand's positioning, key messages, and strategic initiatives.
+    The brief should delve deep into the brand's unique value proposition, target audience, and competitive landscape. 
+    It should also provide clear directives on how the brand should be perceived and the emotions it should evoke.
+    Once you've drafted the brief, it will be reviewed and iterated upon based on feedback from the client and our internal team. 
+    Ensure that the brief is both insightful and actionable, setting a clear path for the brand's journey ahead.
+    Collaborate with the Agency Researcher to ensure that the strategic brief is grounded in solid research and insights.
+    '''
 )
 
-gym_trainer = autogen.AssistantAgent(
-    name="Gym_Trainer",
+agency_researcher = autogen.AssistantAgent(
+    name="Agency_Researcher",
     llm_config={"config_list": config_list},
-    system_message="Gym Trainer. You make sure admin is getting the right training (lifting 4-5 times a week) and eating the right food to get to a 6-pack."
+    system_message=f'''
+    You are the Lead Researcher. 
+    Your primary responsibility is to delve deep into understanding user pain points, identifying market opportunities, and analyzing prevailing market conditions.
+    Using the information from {user_task}, conduct thorough research to uncover insights that can guide our strategic decisions. Your findings should shed light on user behaviors, preferences, and challenges.
+    Additionally, assess the competitive landscape to identify potential gaps and opportunities for our client's brand. 
+    Your research will be pivotal in shaping the brand's direction and ensuring it resonates with its target audience.
+    Ensure that your insights are both comprehensive and actionable, providing a clear foundation for our subsequent strategic initiatives.
+    Share your research findings with the Agency Strategist and Agency Marketer to inform the strategic and marketing initiatives.
+    '''
 )
 
-executive_assistant = autogen.AssistantAgent(
-    name="Executive_Assistant",
+agency_marketer = autogen.AssistantAgent(
+    name="Agency_Marketer",
     llm_config={"config_list": config_list},
-    system_message="Executive Assistant. You make sure the daily work (like project deadlines & daily habits like design and copywriting practice) required by the Admin is done before any of the fun activities."
+    system_message=f'''
+    You are the Lead Marketer. 
+    Your primary role is to take the strategy and insights derived from research and transform them into compelling marketable ideas that resonate with the target audience.
+    Using the strategic direction from {user_task}, craft innovative marketing campaigns, promotions, and initiatives that effectively communicate the brand's value proposition.
+    Your expertise will bridge the gap between strategy and execution, ensuring that the brand's message is not only clear but also captivating. It's essential that your ideas are both impactful and aligned with the brand's overall vision.
+    Collaborate with other teams to ensure a cohesive approach, and always strive to push the boundaries of creativity to set our client's brand apart in the market.
+    Work in tandem with the Agency Manager to ensure that marketing initiatives align with the project's milestones and timelines.
+    '''
 )
 
-planner = autogen.AssistantAgent(
-    name="Planner",
+agency_manager = autogen.AssistantAgent(
+    name="Agency_Manager",
     llm_config={"config_list": config_list},
-    system_message='''Planner. Suggest a plan. Revise the plan based on feedback from admin, Executive Assistant, Fun Manager, until admin approval. Explain the plan first. Be clear which step is performed by an engineer, and which step is performed by a scientist.'''
+    system_message=f'''
+    You are the Project Manager. 
+    Your primary responsibility is to oversee the entire project lifecycle, ensuring that all agents are effectively fulfilling their objectives and tasks on time.
+    Based on the directives from {user_task}, coordinate with all involved agents, set clear milestones, and monitor progress. Ensure that user feedback is promptly incorporated, and any adjustments are made in real-time to align with the project's goals.
+    Act as the central point of communication, facilitating collaboration between teams and ensuring that all deliverables are of the highest quality. Your expertise is crucial in ensuring that the project stays on track, meets deadlines, and achieves its objectives.
+    Regularly review the project's status, address any challenges, and ensure that all stakeholders are kept informed of the project's progress.
+    Coordinate with the Agency Director for periodic reviews and approvals, ensuring that the project aligns with the creative vision.
+    '''
 )
-
-critic = autogen.AssistantAgent(
-    name="Critic",
+agency_director = autogen.AssistantAgent(
+    name="Agency_Director",
     llm_config={"config_list": config_list},
-    system_message="Critic. Double check plan, make sure all objectives from fun manager, executive assistant, and gym trainer are met. Provide feedback."
+    system_message=f'''
+    You are the Creative Director at SCTY. Your primary role is to guide the creative vision of the project, ensuring that all ideas are not only unique and compelling but also meet the highest standards of excellence and desirability.
+    Drawing from the insights of {user_task}, oversee the creative process, inspire innovation, and set the bar for what's possible. Challenge the team to think outside the box and push the boundaries of creativity.
+    Review all creative outputs, provide constructive feedback, and ensure that every piece aligns with the brand's identity and resonates with the target audience. 
+    Your expertise is pivotal in ensuring that our work stands out in the market and leaves a lasting impact.
+    Collaborate closely with all teams, fostering a culture of excellence, and ensuring that our creative solutions are both groundbreaking and aligned with the project's objectives.
+    Engage with the Agency Strategist and Agency Marketer to ensure that the creative outputs align with the strategic direction and marketable ideas.
+    '''
 )
 
 groupchat = autogen.GroupChat(agents=[
-    user_proxy, fun_engineer, executive_assistant, gym_trainer, planner, critic], messages=[], max_round=3)
+    client, agency_researcher, agency_director, agency_marketer, agency_strategist, agency_director], messages=[], max_round=3)
 
 manager = autogen.GroupChatManager(groupchat=groupchat, llm_config={"config_list": config_list})
 
-user_proxy.initiate_chat(
+client.initiate_chat(
     manager,
-    message="""
-    Plan a month-long trip to Bangkok. Include a table of dates and activity. This will give you a list of tasks that need to be done on a particular day.
+    message=f"""
+    {user_task}
     """,
 )
